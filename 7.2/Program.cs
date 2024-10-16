497eb643b8d2b880fae980dfbecf80b0f40c94d5
Horse Tranquilizer <animeberd9@gmail.com>
2024-10-16 09:10:12 -0400

    Update Program.cs

--- 7.2/Program.cs
+++ 7.2/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace _7._2
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _7._2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Random generator = new Random();
+            int randNum;
+            randNum = generator.Next(35);
+            int userguess;
             
+
+
+            for (int i = 1; i <= 10; i = i + 1)
+            {
+                Console.WriteLine("Guess between 1-35");
+                userguess = Convert.ToInt32(Console.ReadLine());
+                if (userguess > randNum)
+                {
+                    Console.WriteLine("Too high");
+                }
+               else if (userguess < randNum)
+                {
+                    Console.WriteLine("too low");
+                }
+               else if (userguess == randNum)
+                {
+                    Console.WriteLine("correct!");
+                }
+
+
+            }
+
         }
     }
 }
